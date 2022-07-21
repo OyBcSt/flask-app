ba67ad9decde3758af2980b5f141421848e24d5e
Lisa L. Lowe <lllowe@ncsu.edu>
2022-07-21 03:21:31 -0400

    modified html

--- myproject.py
+++ myproject.py
@@ -1,8 +1,11 @@
 from flask import Flask, render_template
+from folium.plugins import MarkerCluster
 import folium
 import pandas as pd
 
 app = Flask(__name__)
+app.config['TESTING'] = True
+app.testing = True
 
 @app.route("/")
 def hello():
@@ -26,8 +29,21 @@ def hello():
     fish = station_data["Fishery Station Names"].fillna("")
     Landmarks = station_data["Site Landmarks"].fillna("")
     m = folium.Map(location=[30.50355, -87.98733], zoom_start=10)
+#    marker_cluster = MarkerCluster().add_to(m)
+    folium.TileLayer('openstreetmap').add_to(m)
+    folium.TileLayer('Stamen Terrain').add_to(m)
+    station_group = folium.FeatureGroup(name="Stations")
     for point in range(0, len(locationlist)):
-        folium.Marker(locationlist[point], popup=station_data['StationName'][point] + "\n" + fish[point] + "\n" + Landmarks[point],icon=folium.Icon(color=Colors[point],icon_color="red", icon=Icons[point], angle=0, prefix='fa')).add_to(m)
+        station_group.add_child(folium.Marker(locationlist[point], popup=station_data['StationName'][point] + "\n" + fish[point] + "\n" + Landmarks[point],icon=folium.Icon(color=Colors[point],icon_color="red", icon=Icons[point], angle=0, prefix='fa')))
+    m.add_child(station_group)
+    folium.LayerControl().add_to(m)
+
+#    for point in range(0, len(locationlist)):
+#        folium.Marker(locationlist[point], popup=station_data['StationName'][point] + "\n" + fish[point] + "\n" + Landmarks[point],icon=folium.Icon(color=Colors[point],icon_color="red", icon=Icons[point], angle=0, prefix='fa')).add_to(m)
+#    for point in range(0, len(locationlist)):
+#        info = station_data['StationName'][point] + "\n" + fish[point] + "\n" + Landmarks[point] + ",icon=folium.Icon(color=" + Colors[point] + r",icon_color='red', icon=" + Icons[point] +", angle=0, prefix='fa')"
+#        folium.RegularPolygonMarker(locationlist[point], popup=info).add_to(marker_cluster)
+
     m.save('templates/map.html')
     return render_template('index.html') 
 
@@ -37,4 +53,4 @@ def map():
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', debug=True)
